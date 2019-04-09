ddd019b915e4d48f51a1dd3e5a4c32a92d65303f
walter_ritzel <walter_ritzel@dell.com>
2019-04-09 17:56:53 -0300

    updating the code with last version from Vinicius

--- models/models.py
+++ models/models.py
@@ -1,21 +1,63 @@
-''' Module for Automation models and schemas '''
+''' Module for Services models and schemas '''
 from sqlalchemy import (
     Column, String, Integer, DateTime, func, Sequence)
 from app.database import Base
 
-class Automation(Base):
-    '''Model class for Automation table'''
 
-    __tablename__ = 'automation'
+class Services_Final(Base):
+    '''Model class for Services_Final table'''
 
-    automation_id = Column(Integer(), Sequence('automation_id_seq'), primary_key=True)
-    group_name = Column(String(400), nullable=False, index=True, unique=True)
-    description = Column(String(4000), nullable=True)
-    filter = Column(String(4000), nullable=False)
-    status = Column(String(1), nullable=False, index=True)
-    snow_request = Column(String(400), nullable=True, index=True)
-    last_execution_date = Column(DateTime(), nullable=True, index=True)
-    created_by = Column(String(400), nullable=False, index=True)
-    created_date = Column(DateTime(), server_default=func.now(), index=True)
-    modified_by = Column(String(400), nullable=False, index=True)
-    modified_date = Column(DateTime(), server_default=func.now(), onupdate=func.now(), index=True)
+    __tablename__ = 'services_final'
+
+    svc_id = Column(Integer(), Sequence('service_id_sequence'), primary_key=True)
+    svc_name = Column(String(400), nullable=False, index=True, unique=True)
+    svc_entrypoint = Column(String(400), nullable=False, unique=True)
+    svc_description = Column(String(4000), nullable=True)
+    svc_endpoints = Column(Integer(), nullable=False, index=True)
+    svc_status = Column(String(1), nullable=False, index=True)
+
+    def __repr__(self):
+        return '<Service %r>' % (self.svc_name)
+
+
+class Services_Temp(Base):
+    '''Model class for Services_Temp table'''
+
+    __tablename__ = 'services_temp'
+
+    svc_name = Column(String(400), nullable=False, index=True, unique=True, primary_key=True)
+    svc_entrypoint = Column(String(400), nullable=False, unique=True)
+    svc_description = Column(String(4000), nullable=True)
+    svc_endpoints = Column(Integer(), nullable=False, index=True)
+
+    def __repr__(self):
+        return '<Service_temp %r>' % (self.svc_name)
+
+
+class Endpoints_Final(Base):
+    '''Model class for Endpoints_Final table'''
+
+    __tablename__ = 'endpoints_final'
+
+    end_id = Column(Integer(), Sequence('service_id_sequence'), primary_key=True)
+    svc_id = Column(Integer(), unique=True)
+    end_name = Column(String(400), nullable=False, index=True, unique=True)
+    end_url = Column(String(400), nullable=False, unique=True)
+    end_description = Column(String(4000), nullable=True)
+
+    def __repr__(self):
+        return '<Endpoint %r>' % (self.end_name)
+
+
+class Endpoints_Temp(Base):
+    '''Model class for Endpoints_Temp table'''
+
+    __tablename__ = 'endpoints_temp'
+
+    svc_name = Column(String(400), nullable=False, index=True, unique=True)
+    end_name = Column(String(400), nullable=False, index=True, unique=True, primary_key=True)
+    end_url = Column(String(400), nullable=False, unique=True)
+    end_description = Column(String(4000), nullable=True)
+
+    def __repr__(self):
+        return '<Endpoint_temp %r>' % (self.end_name)
--- retriever.py
+++ retriever.py
@@ -1,28 +1,27 @@
-import json, csv, requests
-from bs4 import BeautifulSoup
+import requests, json, time
 
-page = 1
+#inicio = time.time()
 
-# Cria arquivo .csv
-with open('services-list.csv', 'w', newline='') as fw:
-    #usar o dictwriter
-    f = csv.writer(fw) # newline='' evita linhas espaçadas
-    f.writerow(['List of Services:'])
-    while page < 57:
-        # verificar como trocar este while por pegar o numero de paginas na estrutura que vem da primeira pagina.
-        response = requests.get('http://www.biocatalogue.org/services?page='+str(page))
-        print(response.status_code)  # 200 significa requisição OK para cada página
-        # 200
-        soup = BeautifulSoup(response.text, 'html.parser')
+response = requests.get('https://www.biocatalogue.org/services.json')
+services = response.json()
+print(response.status_code)
 
-        # Lista de <p class='name'>
-        services_list = soup.find_all('p', class_='name')
+pages = services['services']['pages'] + 1
 
-        # Grava no arquivo .csv o nome do serviço, sua URL
-        for service in services_list:
-            services = service.a.get_text()
-            links = ' http://www.biocatalogue.org' + service.a.get('href')
-            f.writerow([services, links])
+with open('services-list.json', 'w') as json_file:
+    for x in range(1, pages):
+        response = requests.get('https://www.biocatalogue.org/services.json?page='+str(x))
+        services = response.json()
+        if response.status_code != 200:
+            print("Erro na requisição")
+        results = services['services']['results']
+        for service in results:
+            details = {
+                'name': service['name'],
+                'resource': service['resource'],
+                'description': service['description']
+            }
+            json.dump(details, json_file)
 
-        page += 1
-
+#fim = time.time()
+#print("Tempo de Execução: ",int(fim - inicio))
